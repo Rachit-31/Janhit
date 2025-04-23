3145bde0997ea443de6a0f0447ed2de9fa5df70e
Rachit-31 <rachitjainjaipur@gmail.com>
2025-04-23 15:28:17 +0530

    signup done for official

--- Server/src/routes/userRoute.js
+++ Server/src/routes/userRoute.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/authMiddleware.js";
 import { loginUser, logout, signupUser } from "../controller/userController.js";
 import { createProblem, rateProblem } from "../controller/problemController.js";
+import { signupOfficial } from "../controller/officialController.js";
 
 
 
@@ -15,5 +16,9 @@ router.route("/logout").post(verifyJWT, logout);
 // problem
 router.route("/createProblem/:userId").post(verifyJWT, createProblem);
 router.route("/problems/:problemId/rate/:userId").post(verifyJWT, rateProblem);
+
+
+// official
+router.route("/signupOfficial").post(signupOfficial)
 
 export default router;
